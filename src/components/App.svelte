f772105f84d3b9e3aa5c390fa74b79ff733bb465
Owen Winkler <owinkler@covermymeds.com>
2020-04-19 13:48:00 -0400

    More CORS-ok sites.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -13,3 +13,11 @@
 <Card site="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" showResponse="no" />
 
 <Card site="http://ss.owenw.com/monitor.svg" showResponse="no" />
+
+<Card site="https://cdn.jsdelivr.net/npm/jquery@3.2.1/src/core.min.js" showResponse="no" />
+
+<Card site="https://ajax.googleapis.com/ajax/libs/mootools/1.6.0/mootools.min.js" showResponse="no" />
+
+<Card site="https://ajax.aspnetcdn.com/ajax/respond/1.4.2/respond.min.js" showResponse="no" />
+
+<Card site="https://code.jquery.com/jquery-3.5.0.slim.min.js" showResponse="no" />
